90564a90c5707f81513dcf72067fd5dcb0e7b899
Raghu C <raghu.9208@gmail.com>
2017-01-19 15:07:00 +0530

    Closes #2 - go back :arrow_backward: ,forward   :arrow_forward: feature

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,15 +2,41 @@ var React = require('react');
 var CityPromptContainer = require('../containers/CityPromptContainer');
 
 var Header = React.createClass({
-    render: function() {
+    contextTypes: {
+        history: React.PropTypes.object.isRequired
+    },
+    handleGoBack: function () {
+        this.context.history.goBack();
+    },
+    handleGoForward: function () {
+        this.context.history.goForward();
+    },
+    render: function () {
+        var buttonClassNames = "mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon ";
+        var goBackClass = this.context.history.canGo(-1) ? buttonClassNames + "show" : buttonClassNames + "hidden";
+        var goForwardClass = this.context.history.canGo(1) ? buttonClassNames + "show" : buttonClassNames + "hidden";
         return (
             <header className="mdl-layout__header">
                 <div className="mdl-layout__header-row">
-                    <span className="mdl-layout-title roboto">Weather-Act</span>
+                    <div className="brand">
+                        <button onClick={this.handleGoBack} className={goBackClass}>
+                            <i className="material-icons" role="presentation">arrow_back</i>
+                            <span className="mdl-button__ripple-container">
+                                <span className="mdl-ripple"></span>
+                            </span>
+                        </button>
+                        <span className="mdl-layout-title roboto">Weather-Act</span>
+                        <button onClick={this.handleGoForward} className={goForwardClass}>
+                            <i className="material-icons" role="presentation">arrow_forward</i>
+                            <span className="mdl-button__ripple-container">
+                                <span className="mdl-ripple"></span>
+                            </span>
+                        </button>
+                    </div>
                     <div className="mdl-layout-spacer"></div>
                     <CityPromptContainer promptType="navigation" />
                 </div>
-            </header>
+            </header >
         )
     }
 });
--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactRouter = require('react-router');
-var HashRouter = ReactRouter.HashRouter;
+var MemoryRouter = ReactRouter.MemoryRouter;
 var Match = ReactRouter.Match;
 var Miss = ReactRouter.Miss;
 var Home = require('../components/Home');
@@ -11,7 +11,7 @@ var DetailContainer = require('../containers/DetailContainer');
 
 function AppContainer() {
     return (
-        <HashRouter>
+        <MemoryRouter>
             <div className="mdl-layout mdl-js-layout">
                 <Header />
                 <main className="mdl-layout__content fullView">
@@ -21,7 +21,7 @@ function AppContainer() {
                     <Miss component={NoMatch} />
                 </main>
             </div>
-        </HashRouter>
+        </MemoryRouter>
     );
 };
 
